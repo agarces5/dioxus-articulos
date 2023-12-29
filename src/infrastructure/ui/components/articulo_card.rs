139af6622fe9f559aa92c177d661a46181d7ce4b
Antonio Garcés <garces080995@gmail.com>
2023-12-29 14:01:53 +0100

    Change css

--- src/infrastructure/ui/components/articulo_card.rs
+++ src/infrastructure/ui/components/articulo_card.rs
@@ -8,47 +8,43 @@ pub fn ArticuloCard<'a>(cx: Scope, articulo: &'a Articulo) -> Element {
     let nombre = articulo.nombre();
     let familia = articulo.familia();
     let detalles = articulo.detalles();
+    let cajas: Vec<&str> = detalles.iter().map(|detalle| detalle.cajtpv()).collect();
     cx.render(rsx! {
         tr {
-            class: "bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600",
-            // td {
-            //     class: "w-4 p-4",
-            //     div {
-            //         class: "flex items-center",
-            //         input {
-            //             id: "checkbox-table-search-1",
-            //             r#type: "checkbox",
-            //             class: "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600",
-            //         }
-            //         label {
-            //             r#for: "checkbox-table-search-1",
-            //             class: "sr-only",
-            //             "checkbox"
-            //         }
-            //     }
-            // }
+            class: "odd:bg-gray-800 even:bg-gray-900 border hover:bg-gray-600",
             td {
-                class: "px-6 py-4",
+                class: "border-x border-white px-6 py-4",
                 "{id}"
             }            
             th {
                 scope: "row",
-                class: "px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white",
+                class: "border-x border-white px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white",
                 "{nombre}"
             }
             td {
-                class: "px-6 py-4",
+                class: "border-x border-white px-6 py-4",
                 "{familia}"
             }            
             td {
-                class: "px-6 py-4",
-                "{detalles:?}"
+                class: "border-x border-white px-0 py-0",
+                table {
+                    class: "w-full h-8 py-0",
+                    tr {
+                        class: "hover:bg-gray-600 py-0",
+                        cajas.iter().map(|caja| rsx! {
+                            td{
+                                class: "border-x border-white px-6 py-4",
+                                "{caja}"
+                            }
+                        } )
+                    }
+                }
             }
             td {
-                class: "px-6 py-4",
+                class: "border-x border-white px-6 py-4",
                 a {
                     href: "",
-                    class: "font-medium text-blue-600 dark:text-blue-500 hover:underline",
+                    class: "font-medium text-blue-400 hover:underline",
                     "Edit"
                 }
             }
